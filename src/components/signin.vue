26f99064837a81aa951ceddb0d79594b8ecc006c
Pragati <pragati.synchsofthq@gmail.com>
2022-04-11 17:13:07 +0530

    Deploy

--- src/components/signin.vue
+++ src/components/signin.vue
@@ -82,8 +82,8 @@ export default {
                 localStorage.setItem('access_token', token)
                 // localStorage.getItem('access_token')
                 if(res.data.status === true){
-                    // this.$router.push('home');
-                    window.location.href = 'home';
+                    this.$router.push('home');
+                    // window.location.href = 'home';
                     
                 }
             }).catch(error => {
