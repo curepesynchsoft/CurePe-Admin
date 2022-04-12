b5a9f919ae66afaeaaf8f36dd336a8ea18d1dda9
Pragati <pragati.synchsofthq@gmail.com>
2022-04-12 10:34:39 +0530

    dashboard

--- src/components/userview.vue
+++ src/components/userview.vue
@@ -4,7 +4,7 @@
         <div class="container-fluid pt-4 px-4">
             <div class="row g-4">
                 <div class="col-sm-6 col-xl-3">
-                    <router-link to="/home/usertable">
+                    <router-link to="/usertable">
                     <div class="bg-light rounded d-flex align-items-center justify-content-between p-4">
                         <i class="fa fa-chart-line fa-3x text-primary"></i>
                         <div class="ms-3">
@@ -15,7 +15,7 @@
                     </router-link>
                 </div>
                 <div class="col-sm-6 col-xl-3">
-                    <router-link to="/home/memberlist">
+                    <router-link to="/membertable">
                         <div class="bg-light rounded d-flex align-items-center justify-content-between p-4">
                         <i class="fa fa-chart-bar fa-3x text-primary"></i>
                         <div class="ms-3">
@@ -277,7 +277,7 @@
 import "../../public/assets/css/style.css"
 import axios from 'axios';
 export default {
-    name: "IndexPage",
+    name: "HomePage",
     data()
     {
         return {
